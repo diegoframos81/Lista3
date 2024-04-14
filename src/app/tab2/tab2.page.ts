97635d9e1f431927dd37b23798af7acdd2c44351
Mateuzo <mateushcandido5@gmail.com>
2024-04-14 16:02:47 -0300

    initial implements from Tab2Page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from './entities/cliente';
+import { Guiches } from './entities/Guiches';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  filaAtendimento: Guiches[] = [];
+  filaUltimosChamados: Guiches[] = [];
+  filaTodosAtendimentosConcluidos: Guiches[] = [];
+
+  tipoAtendimento = ''; //Atributo que puxa o tipo de atendimento solicitado
+  
   constructor() {}
 
+  addToServiceQueue(){
+
+  }
 }
